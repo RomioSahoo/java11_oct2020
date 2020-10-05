7364a570d32f48cf8d36dedc5e50fc319b2d5ae1
prafful <prraful@gmail.com>
2020-10-05 02:44:46 -0700

    java basics

---
+++ FirstHelloWorld.java
@@ -0,0 +1,12 @@
+
+
+public class FirstHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from java!!!!");
+		System.out.println("Hello Again from java!!!!");
+		
+	}
+
+}
